c31e4b552eb8d5d76374298103d229332b19f705
kirby ellingson <hkellingson@gmail.com>
2022-09-28 10:35:22 -0500

    what

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import 'beercss';
+import TopBar from './component/TopBar';
 
 function App() {
-	return <topBar />;
+	return <TopBar />;
 }
 
 export default App;
