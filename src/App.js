e6ab8b2b6286a828270e2e6db59fc97f901bf9c1
kirby ellingson <hkellingson@gmail.com>
2022-09-28 10:26:08 -0500

    Moved websocket to topBar for some reason.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-const socket = new WebSocket('wss://tso-take-home-chat-room.herokuapp.com/');
-//listen for them messages from
-socket.onopen = () => {
-	console.log('Wss is success');
-};
-socket.onmessage = (message) => {
-	console.log('Messages from server:', message);
-};
 
 function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+	return <topBar />;
 }
 
 export default App;
