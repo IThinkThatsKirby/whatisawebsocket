936b8e7090d6cf2b77c4be4e6d91520a9d36af54
kirby ellingson <hkellingson@gmail.com>
2022-09-28 10:35:04 -0500

    added beercss

--- src/component/topBar.js
+++ src/component/topBar.js
@@ -1,6 +1,16 @@
 import React from 'react';
+// Moved websocket to the topBar since It will have the buttons. Its an in house data tool so this should be fine for security.
+// Maybe I could move it to context to and pass some props if I have time.
+const socket = new WebSocket('wss://tso-take-home-chat-room.herokuapp.com/');
+//listen for them messages from
 
 function topBar() {
+	socket.onopen = () => {
+		console.log('Wss is success');
+	};
+	socket.onmessage = (message) => {
+		console.log('Messages from server:', message);
+	};
 	return (
 		<header class='tertiary-container'>
 			<nav>
